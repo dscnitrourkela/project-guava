edce8b4ae88c6a33cdfc07435ef654a62ee9f97b
Abel Mathew <notify.abelmathew@gmail.com>
2020-11-24 16:12:21 +0530

    Setup Docs Automation (#11)
    * Add schema plan v1
    
    * Setup Docs Build

---
+++ planning/v1/save.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+
+function writeMMD(inputname,outputname){
+  const file=fs.readFileSync(inputname, 'utf8')
+  const start=file.indexOf('<div class="mermaid">')+'<div class="mermaid">'.length
+  const end=file.indexOf('</div>')
+  const output=file.slice(start, end);
+  out=output.trim()
+  fs.writeFileSync(outputname,output)  
+}
+writeMMD('flow.html','flow.mmd')
+writeMMD('collectionRelationship.html','collectionRelationship.mmd')
---
+++ planning/v1/schema.js
@@ -0,0 +1,94 @@
+const user ={
+    _id: "String",
+    mail:"mail",
+    name:"String",
+    displayPicture:"String",
+    firebaseId: "String",
+    accessLevel:"String",
+    requests: [request._id],
+    signs:[sign.id],
+    certificates:[
+        {
+            certificateId:certificate._id,
+            title:certificate.title,
+            description:certificate.description,
+        }
+    ],
+    createdAt:"datetime",
+    updatedAt:"datetime",
+    createdBy:"String", //Preferably userid / others
+    updatedBy:"String", //Preferably userid / others
+    schemaVersion:"Integer",
+}
+
+const request={
+    _id:"String",
+    initiater: user._id,
+    title:"String",
+    description:"String",
+    status:"String/Integer",
+    approvers:[
+        {
+            user: user._id,
+            status: "Integer",
+            pixel:["x","y"],
+            scale:"Integer", 
+            approvedAt: "Timestamp"
+        }],
+    certificateInfo:{
+        template:"String",
+        data: "String", //not in database
+    },
+    pixelMap:[{
+        columnName:"ColumnName",
+        pixel:["x","y"],
+        fontSize:"Integer",
+        fontWeight:"Integer"
+    }],
+    font:"String",    
+    createdAt:"datetime",
+    updatedAt:"datetime",
+    createdBy:"String", //Preferably userid / others
+    updatedBy:"String", //Preferably userid / others
+    schemaVersion:"Integer",
+}
+
+const certificate={
+    _id="String",
+    request: request.id,
+    title: request.title,
+    description: request.description,
+    template: "String",
+    font: request.font,
+    mail:user.mail,  
+    pixelMap: [
+        {
+            value:"String",
+            pixel:["x","y"],
+            fontSize:"Integer",
+            fontWeight: "Integer"
+    }],
+    signMap:[{
+        id:sign._id,
+        pixel:["x","y"],
+        scale:"Integer"
+    }],
+    createdAt:"datetime",
+    updatedAt:"datetime",
+    createdBy:"String", //Preferably userid / others
+    updatedBy:"String", //Preferably userid / others
+    schemaVersion:"Integer",
+}
+
+const sign={
+    _id:"String",
+    userId:user._id,
+    name:"String",
+    image:"String",
+    designation:"String",
+    createdAt:"datetime",
+    updatedAt:"datetime",
+    createdBy:"String", //Preferably userid / others
+    updatedBy:"String", //Preferably userid / others
+    schemaVersion:"Integer",
+}
