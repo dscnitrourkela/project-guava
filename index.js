b07778d567285ad0ee737116e99af8c80f8903a2
Ritesh Patil <riteshsp2000@gmail.com>
2020-11-25 01:29:57 +0530

    Add eslint and prettier config files

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 
-
-
 const app = express();
 
 app.use(
-    "/graphql",
-    graphqlHTTP({
-      schema: schema,
-      graphiql: true,
-    }));  
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  }),
+);
 
 app.listen(5000, () => {
-    console.log('now listening for requests on port 5000');
+  console.log('now listening for requests on port 5000');
 });
--- schema/schema.js
+++ schema/schema.js
@@ -3,17 +3,17 @@ const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        status: {
-            type: GraphQLString,
-            resolve(parent, args){
-                return "Welcome to GraphQL"
-            }
-        }
-    }
+  name: 'RootQueryType',
+  fields: {
+    status: {
+      type: GraphQLString,
+      resolve(parent, args) {
+        return 'Welcome to GraphQL';
+      },
+    },
+  },
 });
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+  query: RootQuery,
 });
