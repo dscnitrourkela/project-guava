9475c4c96e898f82298d7574ad7e9d49aef84cf7
Ritesh Patil <riteshsp2000@gmail.com>
2020-11-27 15:20:37 +0530

    Change github action, add eslint and prettier commands

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 
-const app = express()
+const app = express();
 
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHTTP({
     schema,
     graphiql: true,
@@ -13,5 +13,5 @@ app.use(
 );
 
 app.listen(5000, () => {
-  console.log('now listening for requests on port 5000')
+  console.log('now listening for requests on port 5000');
 });
