1bfd3629c2e014d950afb787c3497848ebee93f2
Ritesh Patil <riteshsp2000@gmail.com>
2020-11-28 09:01:10 +0530

    Setup ESLint, Prettier and GitHub Actions for them (#15)
    * Add eslint and prettier config files
    
    * Setup github action for prettier
    
    * Minor Changes to prettier.yml
    
    * fix prettier command error
    
    * Update index.js
    
    * Fix prettier format command to format all current directory files and nested files as well
    
    * Change auto commit commands
    
    * Change github action, add eslint and prettier commands
    
    * Minor changes
    
    * CHange action from pull to push.
    
    Co-authored-by: Abel Mathew <notify.abelmathew@gmail.com>

--- planning/v1/save.js
+++ planning/v1/save.js
@@ -1,13 +1,15 @@
+/* eslint-disable */
+
 const fs = require('fs');
 
-
-function writeMMD(inputname,outputname){
-  const file=fs.readFileSync(inputname, 'utf8')
-  const start=file.indexOf('<div class="mermaid">')+'<div class="mermaid">'.length
-  const end=file.indexOf('</div>')
-  const output=file.slice(start, end);
-  out=output.trim()
-  fs.writeFileSync(outputname,output)  
+function writeMMD(inputname, outputname) {
+  const file = fs.readFileSync(inputname, 'utf8');
+  const start =
+    file.indexOf('<div class="mermaid">') + '<div class="mermaid">'.length;
+  const end = file.indexOf('</div>');
+  const output = file.slice(start, end);
+  // out = output.trim();
+  fs.writeFileSync(outputname, output);
 }
-writeMMD('flow.html','flow.mmd')
-writeMMD('collectionRelationship.html','collectionRelationship.mmd')
+writeMMD('flow.html', 'flow.mmd');
+writeMMD('collectionRelationship.html', 'collectionRelationship.mmd');
