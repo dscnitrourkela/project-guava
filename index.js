bd011b6fd4ff1d1ad41990c5373441ca70b6c033
GitHub Action <action@github.com>
2020-11-28 04:52:24 +0000

    Add Images For Docs and Prettier

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 
-const app = express()
-
-
+const app = express();
 
 app.use(
   '/graphql',
